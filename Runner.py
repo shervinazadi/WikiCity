70f1c3e491d5481431039aab7e7171b71721591e
Shervin Azadi <shervinazadi93@gmail.com>
2018-02-06 02:09:50 +0100

    DataStructure Applied

--- Runner.py
+++ Runner.py
@@ -2,16 +2,14 @@ from bs4 import BeautifulSoup
 import requests
 import Utilities
 
-n = 0
 link = "https://en.wikipedia.org/wiki/Facebook"
 f = requests.get(link)
 
 html_doc = f.text
-
 soup = BeautifulSoup(html_doc, 'html.parser')
 
 print(soup.title.string)
 print()
 
 TestLinks = Utilities.ExtractLinks(soup)
-print(TestLinks)
+print(TestLinks.Articles)
--- Utilities.py
+++ Utilities.py
@@ -1,14 +1,58 @@
-def ExtractLinks(soup):
-    LinkList = []
-    for link in soup.find_all('a'):
-        LinkString = str(link.get('href'))
+import pandas as pd
 
-        if LinkString[:6]=='/wiki/':
-            if LinkString[:11]!='/wiki/File:':
-                if LinkString[:16]!='/wiki/Wikipedia:':
-                    if LinkString[:14]!='/wiki/Special:':
-                        if LinkString[:11]!='/wiki/Help:':
-                            if LinkString[:15]!='/wiki/Category:':
-                                if LinkString[:13]!='/wiki/Portal:':
-                                    LinkList.append(link.get('href'))
-    return LinkList
+class ExtractLinks:
+    def __init__(self, soup):
+
+        ArticleList = []
+        FileList = []
+        WikiList = []
+        SpecialList = []
+        HelpList = []
+        CategoryList = []
+        PortalList = []
+        OutsideList = []
+
+        # Seperating different kinds of links
+
+        for link in soup.find_all('a'):
+            LinkString = str(link.get('href'))
+
+            if LinkString[:6]=='/wiki/':
+
+                if LinkString[:11]=='/wiki/File:':
+                    FileList.append(link.get('href'))
+                elif LinkString[:16]=='/wiki/Wikipedia:':
+                    WikiList.append(link.get('href'))
+                elif LinkString[:14]=='/wiki/Special:':
+                    SpecialList.append(link.get('href'))
+                elif LinkString[:11]=='/wiki/Help:':
+                    HelpList.append(link.get('href'))
+                elif LinkString[:15]=='/wiki/Category:':
+                    CategoryList.append(link.get('href'))
+                elif LinkString[:13]=='/wiki/Portal:':
+                    PortalList.append(link.get('href'))
+                else:
+                    ArticleList.append(link.get('href'))
+            else:
+                OutsideList.append(link.get('href'))
+
+        # Creating Panda Series out of lists
+
+        self.Articles = pd.Series(ArticleList)
+        self.Category = pd.Series(CategoryList)
+        self.Files =  pd.Series(FileList)
+        self.Wiki = pd.Series(WikiList)
+        self.Special = pd.Series(SpecialList)
+        self.Help = pd.Series(HelpList)
+        self.Portal = pd.Series(PortalList)
+        self.Outside = pd.Series(OutsideList)
+
+        # Creating Panda DataFrames out of Series
+        ''' 
+        d = {'Articles' : pd.Series(ArticleList), 'Category' : pd.Series(CategoryList),
+             'Files' : pd.Series(FileList), 'Wiki' : pd.Series(WikiList),
+             'Special' : pd.Series(SpecialList), 'Help' : pd.Series(HelpList),
+             'Portal' : pd.Series(PortalList), 'Outside' : pd.Series(OutsideList), }
+    
+        LinkDataFrame = pd.DataFrame(d)
+        '''
